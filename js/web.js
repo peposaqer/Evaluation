bf0374b690ad7d494de9fdc9dc8dc27384987d9b
Abdallah Saqer <peposaqer@outlook.com>
2020-11-18 12:46:30 +0200

    arbic

--- js/web.js
+++ js/web.js
@@ -1,4 +1,3 @@
-$(".en").hide();
 
 
 $(".navbar-nav>li>a").on("click", function () {
@@ -115,3 +114,131 @@ $(document).ready(function () {
   });
 });
 
+
+$(".en").hide();
+
+//show more button
+$(".arbic").click(function () {
+  $(".arbic").hide();
+  $(".en").show();
+});
+
+//show more button
+$(".en").click(function () {
+  $(".en").hide();
+  $(".arbic").show();
+});
+
+
+
+// Change Languages
+var language;
+// check langauge on loading page
+function loadLang() {
+  let lang_shor_name = localStorage.getItem("language");
+  if (lang_shor_name === "ar") {
+    arabicLang();
+  } else {
+    englishLang();
+  }
+}
+// get Language
+function getLanguage() {
+  localStorage.getItem("language") == null ? setLanguage("en") : false;
+  $.ajax({
+    url: "language/" + localStorage.getItem("language") + ".json",
+    dataType: "json",
+    async: false,
+    dataType: "json",
+    success: function (lang) {
+      language = lang;
+    },
+  });
+}
+
+// set Language
+function setLanguage(lang) {
+  localStorage.setItem("language", lang);
+}
+// set Language
+function arabicLang(lang) {
+  localStorage.setItem("language", lang);
+}
+// change to English
+$(".en").on("click", englishLang);
+// change to Arabic
+$(".arbic").on("click", arabicLang);
+
+
+function arabicLang() {
+  $(".chang").addClass("right-dir");
+  getLanguage();
+  $("#nav_home").text(language.nav_home);
+  $("#nav_about").text(language.nav_about);
+  $("#nav_why").text(language.nav_why);
+  $("#nav_pricing").text(language.nav_pricing);
+  $("#nav_album").text(language.nav_album);
+  $("#nav_testimonials").text(language.nav_testimonials);
+  $("#nav_partners").text(language.nav_partners);
+  $("#nav_contact").text(language.nav_contact);
+  $("#header_h1").text(language.header_h1);
+  $("#header_p").text(language.header_p);
+  $("#about_h1").text(language.about_h1);
+  $("#about_p").text(language.about_p);
+  $("#why_h1").text(language.why_h1);
+  $("#why_p").text(language.why_p);
+  $("#album_h1").text(language.album_h1);
+  $("#album_p").text(language.album_p);
+  $("#Testimonials_h1").text(language.Testimonials_h1);
+  $("#Testimonials_p").text(language.Testimonials_p);
+  $("#clients_h1").text(language.clients_h1);
+  $("#clients_p").text(language.clients_p);
+  $("#Contact_h1").text(language.Contact_h1);
+  $("#contact_p").text(language.contact_p);
+  $("#contact_h3").text(language.contact_h3);
+  $("#contact_copy").text(language.contact_copy);
+  $("#contact_email").attr("placeholder", language.contact_email);
+  $("#contact_textarea").attr("placeholder", language.contact_textarea);
+  $(".Contact ul").css({ "margin-left": "5rem" });
+  $(".header h1").css({ "text-align": "right" });
+  $(".header p").css({ "text-align": "right" });
+
+
+  // Function To Dismiss Progress Ring
+  // dismissProgress();
+}
+
+function englishLang() {
+  $(".chang").removeClass("right-dir");
+  getLanguage();
+  $("#nav_home").text(language.nav_home);
+  $("#nav_about").text(language.nav_about);
+  $("#nav_why").text(language.nav_why);
+  $("#nav_pricing").text(language.nav_pricing);
+  $("#nav_album").text(language.nav_album);
+  $("#nav_testimonials").text(language.nav_testimonials);
+  $("#nav_partners").text(language.nav_partners);
+  $("#nav_contact").text(language.nav_contact);
+  $("#header_h1").text(language.header_h1);
+  $("#header_p").text(language.header_p);
+  $("#about_h1").text(language.about_h1);
+  $("#about_p").text(language.about_p);
+  $("#why_h1").text(language.why_h1);
+  $("#why_p").text(language.why_p);
+  $("#album_h1").text(language.album_h1);
+  $("#album_p").text(language.album_p);
+  $("#Testimonials_h1").text(language.Testimonials_h1);
+  $("#Testimonials_p").text(language.Testimonials_p);
+  $("#clients_h1").text(language.clients_h1);
+  $("#clients_p").text(language.clients_p);
+  $("#Contact_h1").text(language.Contact_h1);
+  $("#contact_p").text(language.contact_p);
+  $("#contact_h3").text(language.contact_h3);
+  $("#contact_copy").text(language.contact_copy);
+  $("#contact_email").attr("placeholder", language.contact_email);
+  $("#contact_textarea").attr("placeholder", language.contact_textarea);
+
+
+  // Function To Dismiss Progress Ring
+  // dismissProgress();
+}
